946ac2d52d075dca9f706a015e58f814d8d96be6
Bhola Yadav <bhya23cse@gmail.com>
2025-06-02 18:07:55 +0530

    new program added

--- margesort.c
+++ margesort.c
@@ -92,7 +92,7 @@ int main()
     cpu_time_used = ((double)(end - start)) / CLOCKS_PER_SEC;
    
     printf("\nSorted array: ");
-    for (int i = 0; i < n; i++)
+    for (i = 0; i < n; i++)
     {
         printf("%d ", arr[i]);
     }
